20e97111c85a885d4d3eee3d8960bf366eff6c9c
Saddam Hussain <saddam008h@gmail.com>
2025-05-19 11:44:04 +0500

    Update retrieveEventData.dart
    added retrieving event data feature

--- lib/database/retrieveEventData.dart
+++ lib/database/retrieveEventData.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+
 class RetrieveEvents {
   Stream<QuerySnapshot> retrieveEventData() {
     final database = FirebaseFirestore.instance;
